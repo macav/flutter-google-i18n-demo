1a29e8824e7cf8304c38a726895f4c6cc7dbb51b
Martin Cavoj <martin.cavoj@renuo.ch>
2020-12-18 10:28:50 +0100

    Upgrade Flutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:i18n_google/main.dart';
+import 'package:flutter_google_i18n_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
